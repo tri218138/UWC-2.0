f5dba71bded2e751f5034d3800dbbd91726e00cc
Trần Minh Nghĩa <85388130+trannghia0997@users.noreply.github.com>
2022-12-07 23:13:38 +0700

    Update collector_controller.py

--- controllers/collector_controller.py
+++ controllers/collector_controller.py
@@ -14,3 +14,26 @@ def auth():
     sign, auth = defineToken(session["idlogin"])
     if not sign:
         return redirect(url_for('main_bp.login'))
+    
+@collector_bp.route('/profile', methods=['GET','POST'])
+def personalInfomation():
+    header = render_template('layout/header.html')
+    sidebar = render_template('layout/sidebar.html')
+    data = dbms.selectUserProfile(auth["idlogin"])
+    data["name"] = data["lname"] + " " +  data["fname"]
+    container = render_template('pages/profile.html', data=data)
+    if request.method == "GET":
+        req = request.args.to_dict()
+        if "mode" in req:
+            if req["mode"] == "edit":
+                container = render_template('pages/profile.html', data=data, mode="edit")
+    elif request.method == "POST":
+        req = request.form.to_dict()
+        if req["request"] == "save":
+            dbms.saveEmployeeInformation(auth["idlogin"], data=req)
+            return redirect(url_for("backofficer_bp.personalInfomation"))
+        elif req["request"] == "cancel":
+            return redirect(url_for("backofficer_bp.personalInfomation"))
+
+    layout = render_template('layout/layout.html',header=header, content=container)
+    return render_template('index.html', content=layout)
