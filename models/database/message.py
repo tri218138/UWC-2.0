6027e4e6ff1b604516cef57bdd1983225aa990df
phminhtu <tu.phamminh2002@hcmut.edu.vn>
2022-12-13 20:25:25 +0700

    du lieu message

--- models/database/message.py
+++ models/database/message.py
@@ -1,4 +1,11 @@
 logData = [
-    {"employee_id": "01393123", "fname": "Tùng", "time": "09/12/2022-01:10:35", "message": "Deadline sắp tới rồi đó cậu"},
-    {"employee_id": "01392183", "fname": "Hậu", "time": "09/12/2022-01:10:40", "message": "Chờ tí, collector Dũng sắp về r"},
+    {"employee_id": "01393123", "fname": "Tùng", "time": "09/12/2022-01:10:35", "message": "MCP bên trường KHTN đầy rồi các cậu"},
+    {"employee_id": "01392183", "fname": "Hậu", "time": "09/12/2022-01:10:40", "message": "Chờ tí, collector Bình sắp về r"},
+    {"employee_id": "341314332", "fname": "Simba", "time": "09/12/2022-01:10:50", "message": "Vậy thì chờ Bình về vậy"},
+    {"employee_id": "01392183", "fname": "Hậu", "time": "09/12/2022-01:20:40", "message": "Bình đã về nhưng không được khỏe cho lắm"},
+    {"employee_id": "341s56436", "fname": "Đăng", "time": "09/12/2022-01:21:10", "message": "Vậy để tôi thu gom chỗ đó"},
+    {"employee_id": "5045335", "fname": "Ngõ", "time": "09/12/2022-01:26:14", "message": "Để tôi, tôi gần chỗ đó hơn"},
+    {"employee_id": "5045335", "fname": "Ngõ", "time": "09/12/2022-01:38:42", "message": "Tôi thu xong rồi nha"},
+    {"employee_id": "135544", "fname": "Bình", "time": "09/12/2022-01:40:03", "message": "Cảm ơn cậu"},
+    {"employee_id": "01393123", "fname": "Tùng", "time": "09/12/2022-01:10:35", "message": "Cảm ơn Ngõ"},
 ]
